c03dcb566cc498ee133b266b84cdce9d5aaca960
Zainab <zainab.gilani@yahoo.com>
2025-03-09 23:23:18 +0400

    New method for divide

--- LearnModules/mysillymath.py
+++ LearnModules/mysillymath.py
@@ -10,4 +10,14 @@ def multiply(num1: float, num2: float) -> float:
     return num1*num2
 #enddef
 
+def divide(num1: float, num2: float) -> float:
+    '''
+    Divides num1 by num2
+    :param num1: first number
+    :param num2: second number
+    :return:
+    '''
+    return num1 / num2
+#enddef
+
 list = [1,2,3]
